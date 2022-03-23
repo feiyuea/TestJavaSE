e2965bb8b972862ba1d61130984808981e5851f2
lh <864843927@qq.com>
2022-03-23 16:22:21 +0800

    添加sex和getset

--- src/com/feiyuea/Student.java
+++ src/com/feiyuea/Student.java
@@ -3,4 +3,29 @@ package com.feiyuea;
 public class Student {
     private  int age;
     private  String name;
+    private  String sex;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 }
